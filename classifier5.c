5b4813d69ae7de676b0ddf23397bc732f800fb27
root <1794703060@qq.com>
2020-03-07 20:12:19 +0800

    222

--- classifier5.c
+++ classifier5.c
@@ -16,52 +16,44 @@ struct http_payload {
 
 static int (*bpf_trace_printk)(const char *fmt, int fmt_size, ...) = (void *)BPF_FUNC_trace_printk;
 
-static inline int __inline__ is_http(struct __sk_buff *skb, __u64 nh_off) {
+static inline int __inline__ is_http(struct __sk_buff *skb, __u64 l2_header_len) {
 	void *data_end = (void*) (long) skb->data_end;
 	void *data = (void*) (long) skb->data;
-	struct iphdr *iph = data + nh_off;
 
-	if (iph + 1 > data_end) {
+	struct iphdr *ip_header_struct = data + l2_header_len;
+	if (ip_header_struct + 1 > data_end) {
 		return 0;
 	}
 
-	if (iph->protocol != IPPROTO_TCP) {
+	if (ip_header_struct->protocol != IPPROTO_TCP) {
 		return 0;
 	}
 
-	__u32 tcp_hlen = 0;
-	__u32 ip_hlen = 0;
-	__u32 poffset = 0;
-	__u32 plength = 0;
-	__u32 ip_total_length = iph->tot_len;
-
-	ip_hlen = iph->ihl << 2;
-
-	if (ip_hlen < sizeof(*iph)) {
+	__u32 ip_header_len = ip_header_struct->ihl << 2;
+	if (ip_header_len < sizeof(*ip_header_struct)) {
 		return 0;
 	}
+	__u32 ip_total_length = ip_header_struct->tot_len;
 
-	struct tcphdr *tcph = data + nh_off + sizeof(*iph);
-
-	if (tcph + 1 > data_end) {
+	struct tcphdr *tcp_header_struct = data + l2_header_len + sizeof(*ip_header_struct);
+	if (tcp_header_struct + 1 > data_end) {
 		return 0;
 	}
-
-	tcp_hlen = tcph->doff << 2;
+	__u32 tcp_header_len = tcp_header_struct->doff << 2;
 
 	struct ipv4_ct_tuple tuple = {};
-	tuple.daddr = iph->daddr;
-	tuple.saddr = iph->saddr;
-	tuple.nexthdr = iph->protocol;
-	if (skb_load_bytes(skb, nh_off + sizeof(*iph), &tuple.dport, 4) < 0){
+	tuple.daddr = ip_header_struct->daddr;
+	tuple.saddr = ip_header_struct->saddr;
+	tuple.nexthdr = ip_header_struct->protocol;
+	if (skb_load_bytes(skb, l2_header_len + sizeof(*ip_header_struct), &tuple.dport, 4) < 0){
 	    return 0;
 	}
 
-	poffset = ETH_HLEN + ip_hlen + tcp_hlen;
-	plength = ip_total_length - ip_hlen - tcp_hlen;
-	if (plength >= 8) {
+	__u32 payload_offset = ETH_HLEN + ip_header_len + tcp_header_len;
+	__u32 payload_length = ip_total_length - ip_header_len - tcp_header_len;
+	if (payload_length >= 8) {
 		__u8 p[8];
-		int ret = skb_load_bytes(skb, poffset, p, 8);
+		int ret = skb_load_bytes(skb, payload_offset, p, 8);
 		if (ret != 0){
 			return 0;
 		}
@@ -77,6 +69,12 @@ static inline int __inline__ is_http(struct __sk_buff *skb, __u64 nh_off) {
 		}
 
 		if ((p[0] == 'H') && (p[1] == 'T') && (p[2] == 'T') && (p[3] == 'P')) {
+			char msg1[] = "srcIP=%d, dstIP=%d\n";
+			bpf_trace_printk(msg1, sizeof(msg1), tuple.saddr, tuple.daddr);
+
+			char msg2[] = "sport=%d, dport=%d\n";
+			bpf_trace_printk(msg2, sizeof(msg2), bpf_htons(tuple.sport), bpf_htons(tuple.dport));
+
 			return 1;
 		}
 	}
@@ -88,22 +86,21 @@ __section("classifier")
 int classification(struct __sk_buff *skb) {
 	void *data_end = (void*) (long) skb->data_end;
 	void *data = (void*) (long) skb->data;
-	struct ethhdr *eth = data;
 
-	__u16 h_proto;
-	__u64 nh_off = 0;
-	nh_off = sizeof(*eth);
+	struct ethhdr *l2_header_struct = data;
+	__u16 l3_proto;
+	__u64 l2_header_len = 0;
+	l2_header_len = sizeof(*l2_header_struct);
 
-	char msg[] = "HTTP GET\n";
-
-	if (data + nh_off > data_end) {
+	if (data + l2_header_len > data_end) {
 		return TC_ACT_OK;
 	}
 
-	h_proto = eth->h_proto;
+	l3_proto = l2_header_struct->h_proto;
 
-	if (h_proto == bpf_htons(ETH_P_IP)) {
-		if (is_http(skb, nh_off) == 1) {
+	if (l3_proto == bpf_htons(ETH_P_IP)) {
+		if (is_http(skb, l2_header_len) == 1) {
+			char msg[] = "HTTP GET\n";
 			bpf_trace_printk(msg, sizeof(msg));
 		}
 	}
